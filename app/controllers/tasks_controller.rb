e5793e98617eaeeb681dce6586a8275f22cea755
hizikataraiden12 <hizikataraiden12@gmail.com>
2020-05-26 11:29:55 +0900

    基本的な昨日の実装の完了戻るならここから

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,4 @@
+
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
@@ -19,6 +20,12 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
     redirect_to tasks_path
   end
 
